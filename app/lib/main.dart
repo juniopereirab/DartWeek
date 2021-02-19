4cdd0aee4a5ddce46f880dcc3d1db5eef91a905c
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-02-19 04:26:08 -0300

    Starting flutter app

---
+++ app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: GestureDetector(
+            child: Text("Contador: $counter"),
+            onTap: () {
+              setState(() {
+                counter++;
+              });
+            }),
+      ),
+    );
+  }
+}
