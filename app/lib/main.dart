679f3490b9e9dfaddccc4199cafab86cddd3d3b0
Dâmaso Júnio <junio.pereirab@gmail.com>
2021-02-26 22:16:46 -0300

    Returning a clean app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,35 @@
-import 'package:app/App.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+      body: Container(),
+    );
+  }
 }
